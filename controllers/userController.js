f5e2c5cf8d69db48c217af9c3dd95a9a600b0893
Michaela Wells <mgwells012@gmail.com>
2023-01-06 00:01:21 -0500

    wrote in the remaining user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,38 +14,94 @@ const userController = {
 
     createUser(req, res) {
         User.create(req.body)
+    .then((dbUserData) => {
+        res.json(dbUserData);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+    },
+
+    singleUser(req,res) {
+        User.findOne({ _id: req.params.userId})
+        .populate('friends')
+        .populate('thoughts')
         .then((dbUserData) => {
-            return User.findOneAndUpdate(
-                {_id: req.body.userId},
-                {$push: {thoughts:dbThoughtData._id} },
-                { new: true }
-            );
-        })
-        .then((dbUserData) => {
-            if(!dbThoughtData) {
-                return res.status(404).json({message: ''})
+            if (!dbUserData) {
+                return res.status(404).json({ message: "there is no user with this ID."});
             }
+            res.json(dbUserData);
         })
         .catch((err) => {
             console.log(err);
-            return res.status(500).json(err);
+            res.status(500).json(err);
         });
     },
-    singleUser(req,res) {
-        User.findOne()
-    },
+
     updateUser(req, res) {
-        User.findOneAndUpdate()
+        User.findOneAndUpdate(
+            { _id: req.params.userId},
+            { }
+        )
+
+        .then((dbUserData) => {
+            if (!dbUserData) {
+                return res.status(404).json({ message: 'There is no user that matches that ID' });
+            }
+            res.json(dbUserData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    
     },
+
     deleteUser(req, res) {
-        User.findOneAndDelete()
+        User.findOneAndDelete({ _id: req.params.userId })
+            .then((dbUserData) => {
+                if (!dbUserData) {
+                    return res.status(404).json({ message: 'We cannot find a user with this ID.' });
+                }
+
+                // i'm missing something here but idk how to write it
+            .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
     },
+    
+
+
     addFriend(req, res) {
-        User.findOneAndUpdate()
+        User.findOneAndUpdate({ _id: req.params.userId }, { $addToSet: { friends: req.params.friendId } }, { new: true })
+        .then((dbUserData) => {
+            if (!dbUserData) {
+            return res.status(404).json({ message: 'There is no user with this ID.' });
+        }
+        res.json(dbUserData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
     },
+
     removeFriend(req, res) {
-        User.findOneAndUpdate()
-    }
-}
+        User.findOneAndUpdate({ _id: req.params.userId }, { $pull: { friends: req.params.friendId } }, { new: true })
+        .then((dbUserData) => {
+            if (!dbUserData) {
+            return res.status(404).json({ message: 'There is no user with this ID.' });
+        }
+        res.json(dbUserData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+};
+
 
 module.exports = userController;
